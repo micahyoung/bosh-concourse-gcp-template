aed07c46f0f46f47f0baee08abf22ebe57cfd2ac
Micah Young <myoung@pivotal.io>
2017-05-09 01:13:57 -0400

    Initial GCP implementation

--- up.sh
+++ up.sh
@@ -9,16 +9,17 @@ if ! [ -d state/ ]; then
 fi
 
 source ./state/env.sh
-AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:?!}
-AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:?"!"}
-AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:?"!"}
+GCP_PROJECT_ID=${GCP_PROJECT_ID:?"!"}
+GCP_PROJECT_DOMAIN=${GCP_PROJECT_DOMAIN:?"!"}
+GCP_SERVICE_ACCOUNT_NAME=${GCP_SERVICE_ACCOUNT_NAME:?"!"}
+GCP_REGION=${GCP_REGION:?"!"}
+GCP_ZONE=${GCP_ZONE:?"!"}
 CONCOURSE_USERNAME=${CONCOURSE_USERNAME:?"!"}
 CONCOURSE_PASSWORD=${CONCOURSE_PASSWORD:?"!"}
 CONCOURSE_TARGET=${CONCOURSE_TARGET:?"!"}
 CONCOURSE_DB_NAME=${CONCOURSE_DB_NAME:?"!"}
 CONCOURSE_DB_ROLE=${CONCOURSE_DB_ROLE:?"!"}
 CONCOURSE_DB_PASSWORD=${CONCOURSE_DB_PASSWORD:?"!"}
-VM_KEYPAIR_NAME=${VM_KEYPAIR_NAME:?"!"}
 DOMAIN=${DOMAIN:?"!"}
 CONCOURSE_BOSH_ENV=${CONCOURSE_BOSH_ENV:?"!"}
 CONCOURSE_DEPLOYMENT_NAME=${CONCOURSE_DEPLOYMENT_NAME:?"!"}
@@ -33,36 +34,58 @@ if ! [ -f bin/bosh ]; then
   chmod +x bin/bosh
 fi
 
+if ! [ -f bin/terraform ]; then
+  curl -L "https://releases.hashicorp.com/terraform/0.9.4/terraform_0.9.4_darwin_amd64.zip" | funzip > bin/terraform
+  chmod +x bin/terraform
+fi
+
 bbl_cmd="bbl --state-dir state/"
 if ! [ -f bin/bbl ]; then
-  curl -JLO "https://github.com/cloudfoundry/bosh-bootloader/releases/download/v3.0.4/bbl-v3.0.4_osx"
-  mv bbl-v3.0.4_osx bin/bbl
+  curl -L "https://github.com/cloudfoundry/bosh-bootloader/releases/download/v3.0.4/bbl-v3.0.4_osx" > bin/bbl
   chmod +x bin/bbl
 fi
 
-export AWS_ACCESS_KEY_ID
-export AWS_SECRET_ACCESS_KEY
-export AWS_DEFAULT_REGION
-if ! aws --version; then
-  pip install --user awscli
+if ! gcloud --version; then
+  brew cask install google-cloud-sdk
 fi
 
-if ! aws ec2 describe-key-pairs | grep -q $VM_KEYPAIR_NAME; then
-  aws ec2 create-key-pair --key-name $VM_KEYPAIR_NAME | jq -r '.KeyMaterial' > state/$VM_KEYPAIR_NAME.pem
-  chmod 600 state/$VM_KEYPAIR_NAME.pem
+if ! gcloud auth list | grep ACTIVE >/dev/null; then
+  gcloud auth login
+fi
+
+if ! gcloud config get-value project | grep $GCP_PROJECT_ID >/dev/null; then
+  gcloud config set project $GCP_PROJECT_ID
+fi
+
+if ! gcloud iam service-accounts list | grep $GCP_SERVICE_ACCOUNT_NAME >/dev/null; then
+  gcloud iam service-accounts create $GCP_SERVICE_ACCOUNT_NAME
+fi
+
+if ! [ -f state/$GCP_SERVICE_ACCOUNT_NAME.key.json ]; then
+  gcloud iam service-accounts keys create --iam-account=$GCP_SERVICE_ACCOUNT_NAME@$GCP_PROJECT_DOMAIN state/$GCP_SERVICE_ACCOUNT_NAME.key.json
+fi
+
+if ! gcloud projects get-iam-policy $GCP_PROJECT_ID | grep $GCP_SERVICE_ACCOUNT_NAME >/dev/null; then
+  gcloud projects add-iam-policy-binding $GCP_PROJECT_ID --member=serviceAccount:$GCP_SERVICE_ACCOUNT_NAME@$GCP_PROJECT_DOMAIN --role='roles/editor'
 fi
 
 if ! [ -f state/bbl-state.json ]; then
   $bbl_cmd \
     up \
-    --aws-access-key-id $AWS_ACCESS_KEY_ID \
-    --aws-secret-access-key $AWS_SECRET_ACCESS_KEY \
-    --aws-region $AWS_DEFAULT_REGION \
-    --iaas aws \
+    --gcp-service-account-key state/$GCP_SERVICE_ACCOUNT_NAME.key.json \
+    --gcp-project-id $GCP_PROJECT_ID \
+    --gcp-region $GCP_REGION \
+    --gcp-zone $GCP_ZONE \
+    --iaas gcp \
   ;
 fi
 
-if ! [ -f state/rsakey.pem ]; then
+if ! [ -f state/bosh.pem ]; then
+  $bbl_cmd ssh-key > state/bosh.pem
+  chmod 600 state/bosh.pem
+fi
+
+if ! [ -f state/$DOMAIN.key ]; then
   openssl req \
     -newkey rsa:2048 \
     -nodes \
@@ -73,11 +96,6 @@ if ! [ -f state/rsakey.pem ]; then
     -multivalue-rdn \
     -out state/$DOMAIN.crt \
   ;
-
-  openssl rsa \
-    -in state/$DOMAIN.key \
-    -out state/rsakey.pem \
-  ;
 fi
 
 if ! $bbl_cmd lbs; then
@@ -85,7 +103,7 @@ if ! $bbl_cmd lbs; then
     create-lbs \
     --type concourse \
     --cert state/$DOMAIN.crt \
-    --key state/rsakey.pem
+    --key state/$DOMAIN.key
 fi
 
 DIRECTOR_ADDRESS=$($bbl_cmd director-address)
@@ -105,11 +123,11 @@ if ! bosh env --environment $DIRECTOR_ADDRESS; then
   ;
 fi
 
-if ! bosh stemcells -e $CONCOURSE_BOSH_ENV | grep -q bosh-aws-xen-hvm-ubuntu-trusty-go_agent; then
-  bosh upload-stemcell -e $CONCOURSE_BOSH_ENV https://s3.amazonaws.com/bosh-core-stemcells/aws/bosh-stemcell-3363.9-aws-xen-hvm-ubuntu-trusty-go_agent.tgz
+if ! bosh stemcells -e $CONCOURSE_BOSH_ENV | grep -q bosh-google-kvm-ubuntu-trusty-go_agent; then
+  bosh upload-stemcell -e $CONCOURSE_BOSH_ENV https://s3.amazonaws.com/bosh-core-stemcells/google/bosh-stemcell-3363.20-google-kvm-ubuntu-trusty-go_agent.tgz
 fi
 
-CONCOURSE_LBS_DOMAIN=$($bbl_cmd lbs | sed 's/.*\[\(.*\)\]/\1/')  # Format: Concourse LB: stack-bbl-Concours-1RABRZ7DBDC7F [stack-bbl-Concours-1RABRZ7DBDC7F-585187859.us-west-2.elb.amazonaws.com]
+CONCOURSE_LBS_DOMAIN=$($bbl_cmd lbs | sed 's/.*: \(*\)/\1/')  # Format: Concourse LB: 1.2.3.4
 if ! [ -f state/concourse-creds.yml ]; then
   # from https://github.com/cloudfoundry/bosh-bootloader/blob/master/docs/concourse_aws.md
   cat > state/concourse-creds.yml <<EOF
@@ -120,7 +138,7 @@ concourse_basic_auth_password: $CONCOURSE_PASSWORD
 concourse_atc_db_name: $CONCOURSE_DB_NAME
 concourse_atc_db_role: $CONCOURSE_DB_ROLE
 concourse_atc_db_password: $CONCOURSE_DB_PASSWORD
-concourse_vm_type: t2.small
+concourse_vm_type: n1-standard-1
 concourse_worker_vm_extensions: 50GB_ephemeral_disk
 concourse_web_vm_extensions: lb
 concourse_db_disk_type: 5GB
@@ -132,7 +150,7 @@ if ! bosh deployments -e $CONCOURSE_BOSH_ENV | grep -q $CONCOURSE_DEPLOYMENT_NAM
     --non-interactive \
     --environment $CONCOURSE_BOSH_ENV \
     --deployment $CONCOURSE_DEPLOYMENT_NAME \
-    --vars-file state/concourse-creds.yml \
+    --vars-store state/concourse-creds.yml \
     concourse-deployment.yml \
   ;
 fi
